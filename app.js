add9d718aa4f3cdec268b1ac2522ebaa301824e9
ahmed-tarek <c.tarek2015@gmail.com>
2026-07-23 13:04:26 +0300

    moreee-practice

--- app.js
+++ app.js
@@ -27,10 +27,6 @@ let color = colors[1];
 
 console.log(color);
 
-for (let idx = 0; idx < movies.length; idx++) {
-  console.log(movies[idx]);
-}
-
 let tasks = [];
 function addTask(task) {
   tasks.push(task);
@@ -55,3 +51,71 @@ addTask("Build a mini project");
 listTasks();
 removeTask(0);
 listTasks();
+
+console.log(movies);
+console.log("=========================");
+const msg = "my favourite movie is";
+
+function printMsg(movie) {
+  return `My favorite movie is ${movie}`;
+}
+
+for (let idx = 0; idx < movies.length; idx++) {
+  const movie = movies[idx];
+  console.log(printMsg(movie));
+}
+
+console.log("=========================");
+
+for (let movie of movies) {
+  console.log(printMsg(movie));
+}
+
+console.log("=========================");
+
+movies.forEach(printMsg);
+
+movies.forEach(function (movie) {
+  console.log(`${msg}: ${movie}`);
+});
+
+movies.forEach((movie) => {
+  console.log(movie);
+});
+
+console.log("=========================");
+
+movies.forEach((movie, index) => {
+  if (index === 2) console.log("that is it");
+  console.log(index + 1 + "-" + movie);
+});
+
+const alphaBet = "abcd";
+console.log(alphaBet.split(""));
+
+console.log("=========================");
+
+const books = ["The Shining", "Pride and Prejudice"];
+console.log(books[1]);
+for (book of books) {
+  console.log(book);
+}
+
+books.forEach(function (book, index) {
+  console.log(index + book);
+});
+
+const nums = [1, 2, 3, 4, 5];
+let total = 0;
+nums.forEach(function (num) {
+  total += num;
+});
+console.log(total);
+
+function max5(nr1, nr2, nr3, nr4, nr5) {
+  const nums = [nr1, nr2, nr3, nr4, nr5];
+  nums.sort();
+  return nums.at(-1);
+}
+
+console.log(max5(5, 1, 9, 7, 6));
